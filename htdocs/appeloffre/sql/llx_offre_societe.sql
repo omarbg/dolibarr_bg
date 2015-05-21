24070696e57eb8d070961bad3d2ed8385e21f8c7
Admin <Admin@Admin-PC>
2015-05-21 22:36:01 +0100

    table offre societe pour sauvegarde l’adjudicataire

---
+++ htdocs/appeloffre/sql/llx_offre_societe.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Jeu 21 Mai 2015 à 23:33
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `dolibarr_fr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `llx_offre_societe`
+--
+
+CREATE TABLE IF NOT EXISTS `llx_offre_societe` (
+  `id_offre` int(11) NOT NULL,
+  `id_soc` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
